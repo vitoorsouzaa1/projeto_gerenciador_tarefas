44843b17851692da04fa6a73ea1b9133ff163f20
vitoorsouzaa1 <vitor.souza407@yahoo.com.br>
2021-12-21 21:26:32 -0300

    feat: add a rout to find tasks by ID

--- backend/index.js
+++ backend/index.js
@@ -20,6 +20,20 @@ app.get("/tasks", async (req, res) => {
     }
 });
 
+app.get("/tasks/:id", async (req, res) => {
+    try {
+        const taskId = req.params.id;
+
+        const task = await taskModel.findById();
+        if (!task) {
+            return res.status(404).send("Essa tarefa não foi encontrada!");
+        }
+        return res.status(200).send(task);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
 app.post("/tasks", async (req, res) => {
     try {
         const newTask = new taskModel(req.body);
