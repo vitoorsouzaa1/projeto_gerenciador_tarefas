85e8d792cad3f63ab1bfa0079f77dd1e2a8f0fc6
vitoorsouzaa1 <vitor.souza407@yahoo.com.br>
2021-12-22 21:56:03 -0300

    feat: fetch tasks from back-end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 import TaskItem from "./components/TaskItem";
 
 const App = () => {
@@ -15,6 +17,20 @@ const App = () => {
         },
     ]);
 
+    const fetchTasks = async () => {
+        try {
+            const { data } = await axios.get("http://localhost:8000/tasks");
+
+            setTasks(data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        fetchTasks();
+    }, []);
+
     return (
         <>
             {tasks.map((task) => (
@@ -25,48 +41,3 @@ const App = () => {
 };
 
 export default App;
-
-// **************************** COMPONENT TO CLASS ****************************
-
-// import React from "react";
-// import TaskItem from "./components/TaskItem";
-
-// class App extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.handleStateChange = this.handleStateChange.bind(this);
-//         this.state = {
-//             tasks: [
-//                 {
-//                     id: "1",
-//                     description: "Estudar",
-//                     isCompleted: false,
-//                 },
-//                 {
-//                     id: "2",
-//                     description: "Ler",
-//                     isCompleted: true,
-//                 },
-//             ],
-//         };
-//     }
-
-//     handleStateChange() {
-//         this.setState({
-//             tasks: [],
-//         });
-//     }
-
-//     render() {
-//         return (
-//             <>
-//                 {this.state.tasks.map((task) => (
-//                     <TaskItem key={task.id} task={task} />
-//                 ))}
-//                 <button onClick={this.handleStateChange}>Limpar Tarefas</button>
-//             </>
-//         );
-//     }
-// }
-
-// export default App;
--- frontend/src/components/TaskItem.js
+++ frontend/src/components/TaskItem.js
@@ -8,22 +8,3 @@ const TaskItem = ({ task }) => {
 };
 
 export default TaskItem;
-
-// **************************** COMPONENT TO CLASS ****************************
-
-// import React from "react";
-
-// class TaskItem extends React.Component {
-//     render() {
-//         const { task } = this.props;
-
-//         return (
-//             <>
-//                 <h1>{task.description}</h1>
-//                 <p>{task.isCompleted ? "Completa" : "Não completa"}</p>
-//             </>
-//         );
-//     }
-// }
-
-// export default TaskItem;
