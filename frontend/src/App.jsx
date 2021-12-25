f2d39bba94828647b2622181d5f5c7d3e12582ec
vitoorsouzaa1 <vitor.souza407@yahoo.com.br>
2021-12-25 20:23:12 -0300

    feat: add app container

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import Tasks from "./components/Tasks";
+import Sidebar from "./components/Sidebar";
+
+import "./App.scss";
 
 const App = () => {
     return (
-        <>
+        <div className="app-container">
+            <Sidebar />
             <Tasks />
-        </>
+        </div>
     );
 };
 
